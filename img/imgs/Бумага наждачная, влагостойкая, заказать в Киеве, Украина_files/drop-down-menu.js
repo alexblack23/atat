340b65934f2553204428a5ebbae8b15b73f83696
Oleksandr Chornyi <oleksandr.chornyi.23@gmail.com>
2018-10-16 12:54:31 +0300

    eeew

--- img/imgs/Бумага наждачная, влагостойкая, заказать в Киеве, Украина_files/drop-down-menu.js
+++
@@ -1,9 +0,0 @@
-function showSubmenu(pObj) {
-	var cClass = pObj.parentNode.className;
-	if (cClass.indexOf("current") > -1) {
-		pObj.parentNode.className = cClass.replace("current", "");
-	}
-	else {
-		pObj.parentNode.className += " current";
-	}
-}
